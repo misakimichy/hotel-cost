ffbf90dc1965078453411f7751be89733fa6a679
Misaki Koonce <misaki.koonce@gmail.com>
2019-09-23 00:21:07 -0700

    Show special message if it's summer and weekend.

--- HotelCost.cs
+++ HotelCost.cs
@@ -12,5 +12,14 @@ class HotelCost
 
     bool summer = season == "summer";
     bool weekend = dayOfWeek == "weekend";
+
+    if(summer && weekend)
+    {
+      Console.WriteLine("Your stay is going to be expensive. It's both peak travel season AND the weekend.");
+    }
+    else
+    {
+      Console.WriteLine("Your stay might be expensive, but it could be worse!");
+    }
   }
 }
